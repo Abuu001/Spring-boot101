17afc195b62345cb6e6f3e08f97b327117b20fef
Abuu001 <abulugz@gmail.com>
2021-05-08 11:22:19 +0300

    md

--- src/main/java/com/lugonzo/Springboot101/Dao/MongoStudentDaoImpl.java
+++ src/main/java/com/lugonzo/Springboot101/Dao/MongoStudentDaoImpl.java
@@ -1,2 +1,43 @@
-package com.lugonzo.Springboot101.Dao;public class MongoStudentDaoImpl {
+package com.lugonzo.Springboot101.Dao;
+
+import com.lugonzo.Springboot101.Entity.Student;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Repository
+@Qualifier("mongoData")
+public class MongoStudentDaoImpl implements FakeStudentDao{
+
+    @Override
+    public Collection<Student> getAllStudents() {
+        return new ArrayList<Student>(){
+            {
+                add(new Student(1,"Abraham","Maths"));
+            }
+        };
+    }
+
+    @Override
+    public Student getStudentById(int id) {
+        return null;
+    }
+
+    @Override
+    public void removeStudentById(int id) {
+
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+
+    }
+
+    @Override
+    public void insertStudent(Student student) {
+
+    }
 }
