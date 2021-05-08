f37e656250fda6bc876613cef117426f0926bc40
Abuu001 <abulugz@gmail.com>
2021-05-08 22:01:45 +0300

    person repository

--- src/main/java/com/lugonzo/Springboot101/Dao/PersonDao.java
+++ src/main/java/com/lugonzo/Springboot101/Dao/PersonDao.java
@@ -1,2 +1,25 @@
-package com.lugonzo.Springboot101.Dao;public interface PersonDao {
+package com.lugonzo.Springboot101.Dao;
+
+import com.lugonzo.Springboot101.Model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person){
+        UUID id = UUID.randomUUID();
+        return insertPerson(id,person);
+    };
+
+    List<Person>  selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+
+    int deletePersonById(UUID id);
 }
