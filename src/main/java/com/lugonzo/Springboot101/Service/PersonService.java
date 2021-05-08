67ca2ee1bda10270a10d36950015b0c0c366ec18
Abuu001 <abulugz@gmail.com>
2021-05-08 22:02:41 +0300

    md

--- src/main/java/com/lugonzo/Springboot101/Service/PersonService.java
+++ src/main/java/com/lugonzo/Springboot101/Service/PersonService.java
@@ -1,2 +1,42 @@
-package com.lugonzo.Springboot101.Service;public class PersonService {
+package com.lugonzo.Springboot101.Service;
+
+import com.lugonzo.Springboot101.Dao.PersonDao;
+import com.lugonzo.Springboot101.Model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public  int addPerson(Person person ){
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople(){
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+
+    public  int deletePerson(UUID id){
+        return personDao.deletePersonById(id);
+    }
+
+    public  int updatePerson(UUID id,Person newPerson){
+        return personDao.updatePersonById(id ,newPerson);
+    }
 }
