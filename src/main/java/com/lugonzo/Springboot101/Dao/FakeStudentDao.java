9f551ee5133757475a5e2da3a00d7f7f453fccee
Abuu001 <abulugz@gmail.com>
2021-05-08 11:21:40 +0300

    student interface

--- src/main/java/com/lugonzo/Springboot101/Dao/FakeStudentDao.java
+++ src/main/java/com/lugonzo/Springboot101/Dao/FakeStudentDao.java
@@ -1,2 +1,17 @@
-package com.lugonzo.Springboot101.Dao;public interface FakeStudentDao {
+package com.lugonzo.Springboot101.Dao;
+
+import com.lugonzo.Springboot101.Entity.Student;
+
+import java.util.Collection;
+
+public interface FakeStudentDao {
+    Collection<Student> getAllStudents();
+
+    Student getStudentById(int id);
+
+    void removeStudentById(int id);
+
+    void updateStudent(Student student);
+
+    void insertStudent(Student student);
 }
