f2a97650a3ca255d7427c71ff0aac807d5fb8a97
Abuu001 <abulugz@gmail.com>
2021-06-13 15:40:26 +0300

    user properties

--- src/main/java/com/lugonzo/Springboot101/Entity/User.java
+++ src/main/java/com/lugonzo/Springboot101/Entity/User.java
@@ -1,2 +1,22 @@
-package com.lugonzo.Springboot101.Entity;public class User {
+package com.lugonzo.Springboot101.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_credentials")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
 }
+///insert INTO user_credentials VALUES(1,'password','USER','Abu');
