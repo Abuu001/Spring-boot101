79c552c9ea16413ba16002335895a9a0e2a83efd
Abuu001 <abulugz@gmail.com>
2021-06-13 15:40:03 +0300

    Configured security

--- src/main/java/com/lugonzo/Springboot101/Security/AppSecurityConfig.java
+++ src/main/java/com/lugonzo/Springboot101/Security/AppSecurityConfig.java
@@ -1,2 +1,57 @@
-package com.lugonzo.Springboot101.Security;public class AppSecurityConfig {
+package com.lugonzo.Springboot101.Security;
+
+import ch.qos.logback.core.joran.action.NOPAction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EnableWebSecurity
+@Configuration
+@EnableOAuth2Sso  //anotation to enable auth eg google ,fg,github
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+         http
+                 .csrf().disable()
+                 .authorizeRequests().antMatchers("/login").permitAll()
+                 .anyRequest().authenticated();
+    }
+
+    /*    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder().username("Naomi").password("password").roles("USER").build());
+        users.add(User.withDefaultPasswordEncoder().username("linda").password("password123").roles("ADMIN").build());
+
+        return new InMemoryUserDetailsManager(users);
+    }*/
 }
--- src/main/java/com/lugonzo/Springboot101/Service/UserDetailsService.java
+++ src/main/java/com/lugonzo/Springboot101/Service/UserDetailsService.java
@@ -1,2 +1,26 @@
-package com.lugonzo.Springboot101.Service;public class UserDetailsService {
+package com.lugonzo.Springboot101.Service;
+
+import com.lugonzo.Springboot101.Entity.User;
+import com.lugonzo.Springboot101.Entity.UserPrincipal;
+import com.lugonzo.Springboot101.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(s);
+        if(user==null){
+            throw new UsernameNotFoundException("User 404");
+        }
+
+        return new UserPrincipal(user);
+    }
 }
