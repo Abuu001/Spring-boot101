f060e28fad48f69907c4689cf46765945334da8b
Abuu001 <abulugz@gmail.com>
2021-06-13 15:41:06 +0300

    user repository

--- src/main/java/com/lugonzo/Springboot101/Repository/UserRepository.java
+++ src/main/java/com/lugonzo/Springboot101/Repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.lugonzo.Springboot101.Repository;
 
-public interface UserRepository {
+import com.lugonzo.Springboot101.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository  extends JpaRepository<User,Long> {
+    User findByUsername(String userName);
 }
